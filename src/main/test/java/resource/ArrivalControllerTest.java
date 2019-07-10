5825dda1946543400030f55bc3259e472df3c0fd
Julius Cerniauskas <julius.cerniauskas.mif@gmail.com>
2019-07-10 04:26:29 +0300

    introduced more test for ArrivalController

--- src/main/test/java/resource/ArrivalControllerTest.java
+++ src/main/test/java/resource/ArrivalControllerTest.java
@@ -8,24 +8,24 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import service.arrival.ArrivalService;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 class ArrivalControllerTest {
     private static final String ID = "id";
     private static final String PORT = "port1";
-    private static final LocalDateTime DATE  = LocalDateTime.of(LocalDate.of(2000,2,2), LocalTime.of(2,50));
+    private static final Date DATE  = new Date(1999,2,2);
     private static final String ID_2 = "id2";
     private static final String PORT_2 = "port2";
-    private static final LocalDateTime DATE_2  = LocalDateTime.of(LocalDate.of(1000,1,1), LocalTime.of(2,50));
+    private static final Date DATE_2 = new Date(1999,2,2);
 
     @Mock
     private ArrivalService arrivalService;
@@ -36,45 +36,47 @@ class ArrivalControllerTest {
     private Arrival arrival;
     private Arrival arrival2;
 
-//    @BeforeEach
-//    public void init()throws Exception {
-//        MockitoAnnotations.initMocks(this);
-//
-//        arrival = new Arrival();
-//        arrival.setDate(DATE);
-//        arrival.setPort(PORT);
-//        arrival.setId(ID);
-//
-//        arrival2 = new Arrival();
-//        arrival2.setDate(DATE_2);
-//        arrival2.setPort(PORT_2);
-//        arrival2.setId(ID_2);
-//
-//        when(arrivalService.findAll()).thenReturn(Arrays.asList(arrival, arrival2));
-//        when(arrivalService.findById(anyString())).thenReturn(arrival);
-//    }
-//
-//    @Test
-//    void shouldGetArrivalById() {
-//        Arrival result = arrivalController.getById(ID);
-//
-//        assertNotNull(result);
-//        assertEquals(ID, result.getId());
-//        assertEquals(DATE, result.getDate());
-//        assertEquals(PORT, result.getPort());
-//    }
-//
-//    @Test
-//    void shouldReturnAllArrivals() {
-//        List<Arrival> arrivals = arrivalController.findAll();
-//
-//        assertEquals(2, arrivals.size());
-//        assertEquals(ID, arrivals.get(0).getId());
-//        assertEquals(ID_2, arrivals.get(1).getId());
-//    }
+    @BeforeEach
+    public void init()throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        arrival = new Arrival();
+        arrival.setDate(DATE);
+        arrival.setPort(PORT);
+        arrival.setId(ID);
+
+        arrival2 = new Arrival();
+        arrival2.setDate(DATE_2);
+        arrival2.setPort(PORT_2);
+        arrival2.setId(ID_2);
+    }
+
+    @Test
+    void shouldGetArrivalById() {
+        when(arrivalService.findById(anyString())).thenReturn(arrival);
+
+        Arrival result = arrivalController.getById(ID);
+
+        assertNotNull(result);
+        assertEquals(ID, result.getId());
+        assertEquals(DATE, result.getDate());
+        assertEquals(PORT, result.getPort());
+    }
+
+    @Test
+    void shouldReturnAllArrivals() {
+        when(arrivalService.findAll()).thenReturn(Arrays.asList(arrival, arrival2));
+
+        List<Arrival> arrivals = arrivalController.findAll();
+
+        assertEquals(2, arrivals.size());
+        assertEquals(ID, arrivals.get(0).getId());
+        assertEquals(ID_2, arrivals.get(1).getId());
+    }
 
     @Test
     void shouldCreateArrival() {
+
     }
 
     @Test
@@ -83,5 +85,12 @@ class ArrivalControllerTest {
 
     @Test
     void shouldDeleteArrivalById() {
+        doNothing().when(arrivalService).deleteById(anyString());
+        when(arrivalService.findById(anyString())).thenReturn(null);
+
+        arrivalController.deleteById(ID);
+        Arrival arrival = arrivalController.getById(ID);
+
+        assertNull(arrival);
     }
 }
