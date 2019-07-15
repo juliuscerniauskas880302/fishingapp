477a8a51652a86f689bd9e7032dc9e9e48e89236
Julius Cerniauskas <julius.cerniauskas.mif@gmail.com>
2019-07-15 21:15:46 +0300

    fixed variable paths

--- src/main/java/common/ApplicationVariables.java
+++ src/main/java/common/ApplicationVariables.java
@@ -5,12 +5,12 @@ public class ApplicationVariables {
      * Controller paths
      */
     public static final String APPLICATION_PATH = "api";
-    public static final String CATCH_PATH = "catches";
-    public static final String CONFIG_PATH = "configs";
-    public static final String DEPARTURE_PATH = "departures";
-    public static final String END_OF_FISHING_PATH = "ends";
-    public static final String LOGBOOK_PATH = "logs";
-    public static final String ARRIVAL_PATH = "arrivals";
+    public static final String CATCH_PATH = "/catches";
+    public static final String CONFIG_PATH = "/configs";
+    public static final String DEPARTURE_PATH = "/departures";
+    public static final String END_OF_FISHING_PATH = "/ends";
+    public static final String LOGBOOK_PATH = "/logs";
+    public static final String ARRIVAL_PATH = "/arrivals";
 
     /**
      * Properties file path
