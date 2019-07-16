b84885669c6d84ec210de686bb0aa2dd86f62239
Julius Cerniauskas <julius.cerniauskas.mif@gmail.com>
2019-07-16 07:24:25 +0300

    Logbook mapper in progress.

--- src/main/java/camel/ZipParserRouterBuilder.java
+++ src/main/java/camel/ZipParserRouterBuilder.java
@@ -2,12 +2,25 @@ package camel;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.zipfile.ZipSplitter;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import javax.ejb.Stateless;
 import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 @Stateless
 public class ZipParserRouterBuilder extends RouteBuilder {
+
+    private Map<String, Object> logbookMap = new HashMap<>();
+    private Map<String, Object> claseesMap = new HashMap<>();
+    private Map<String, Object> catchesMap = new HashMap<>();
 
     @Override
     public void configure() throws Exception {
@@ -21,7 +34,8 @@ public class ZipParserRouterBuilder extends RouteBuilder {
                                         .pollEnrich("file:c:/datafiles/data_import/arrival").process().exchange(exchange ->
                                             {
                                                 File file = exchange.getIn().getBody(File.class);
-                                                System.out.println("FILE PATH = "+ file);
+                                                buildArrival(file.getAbsolutePath());
+
                                             })
 
                                 .endChoice()
@@ -64,6 +78,23 @@ public class ZipParserRouterBuilder extends RouteBuilder {
 
                         .end()
                 .end();
+    }
+
+    private void buildArrival(String filePath) {
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get(filePath));
+            CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT
+                    .withFirstRecordAsHeader()
+                    .withIgnoreHeaderCase()
+                    .withTrim())) {
+            for(CSVRecord record: parser){
+                String id = record.get("id");
+                String logbook_id = record.get("logbook_id");
+                String port = record.get("port_id");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
