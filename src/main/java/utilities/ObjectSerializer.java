c7e82e76274a3ca74b9bb1724aa6448fb6977327
Julius Cerniauskas <julius.cerniauskas.mif@gmail.com>
2019-07-30 12:38:24 +0300

    Simplify try/catch block on ObjectSerializer class.

--- src/main/java/utilities/ObjectSerializer.java
+++ src/main/java/utilities/ObjectSerializer.java
@@ -1,5 +1,8 @@
 package utilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -8,42 +11,27 @@ import java.io.ObjectOutputStream;
 import java.util.Base64;
 
 public class ObjectSerializer<T extends Object> {
+    private static final Logger LOG = LogManager.getLogger(ObjectSerializer.class);
 
     public String serialize(T object) {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ObjectOutputStream objectOutputStream = null;
-        try {
-            objectOutputStream = new ObjectOutputStream(outputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream)) {
             objectOutputStream.writeObject(object);
+            return Base64.getEncoder().encodeToString(outputStream.toByteArray());
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Error occur during serialization. {}. {}.", e.getCause(), e.getMessage());
         }
-        return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+        return null;
     }
 
     public T deSerialize(String serialized, Class<T> tClass) {
         byte[] data = Base64.getDecoder().decode(serialized);
-        ObjectInputStream objectInputStream = null;
-        try {
-            objectInputStream = new ObjectInputStream(new ByteArrayInputStream(data));
-        } catch (IOException e) {
-            e.printStackTrace();
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new ByteArrayInputStream(data))) {
+            T object = (T) objectInputStream.readObject();
+            return object;
+        } catch (Exception e) {
+            LOG.error("Error occur during serialization. {}. {}.", e.getCause(), e.getMessage());
         }
-        T object = null;
-        try {
-            object = (T) objectInputStream.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            objectInputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return object;
+        return null;
     }
 }
